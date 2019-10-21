c196a1f44e727482ee374bb0d10ce196d3ce8a63
3goliad <3goliad@gmail.com>
2019-10-21 12:03:34 -0400

    trying to get a grid

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick, onMouseDown)
 
 
 
@@ -39,6 +40,7 @@ init _ =
 
 type Msg
     = Reset
+    | Drag
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -61,18 +63,73 @@ subscriptions _ =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "Switcher" ]
-        , button [ onClick Reset ]
-            [ text (resetText model) ]
-        ]
+    let
+        header =
+            h1 [] [ text "Switcher" ]
+
+        resetButton t =
+            button [ onClick Reset ] [ text t ]
+    in
+    main_ []
+        (case model of
+            Unstarted ->
+                [ header, resetButton "Begin" ]
+
+            Started ->
+                [ header, gameBoard, resetButton "Reset" ]
+        )
 
 
-resetText : Model -> String
-resetText model =
-    case model of
-        Started ->
-            "Reset"
+gameBoard =
+    div [ class "gameboard" ] pieces
 
-        Unstarted ->
-            "Begin"
+
+piece : Int -> Int -> Html Msg
+piece x y =
+    div [ class "piece" ]
+        [ text (String.fromInt x ++ ", " ++ String.fromInt y) ]
+
+
+row : Int -> Html Msg
+row y =
+    div [ class ("row-" ++ intName y) ]
+        (List.map (\x -> piece x y) (List.range 1 9))
+
+
+pieces : List (Html Msg)
+pieces =
+    List.map row (List.range 1 9)
+
+
+intName : Int -> String
+intName i =
+    case i of
+        1 ->
+            "one"
+
+        2 ->
+            "two"
+
+        3 ->
+            "three"
+
+        4 ->
+            "four"
+
+        5 ->
+            "five"
+
+        6 ->
+            "six"
+
+        7 ->
+            "seven"
+
+        8 ->
+            "eight"
+
+        9 ->
+            "nine"
+
+        _ ->
+            "unk"
